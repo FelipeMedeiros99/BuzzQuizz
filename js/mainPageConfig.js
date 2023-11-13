2649d6539dab6b4a2d935221bf7d8cf86860e3ef
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2023-11-13 03:10:56 -0300

    página inicial dinâmica funcionando, carregando elementos automaticamente. css integrados

---
+++ js/mainPageConfig.js
@@ -0,0 +1,55 @@
+// ============== vars =========================== // 
+export const SERVERLINK ="https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+export let ALLQUIZZESLIST = []
+let requestReceived = ''
+
+
+// ============== functions =========================== //
+
+// function to add the quizzes to the main page --------------
+
+function stopWaiting(){
+    clearInterval(requestReceived)
+}
+
+function insertQuizzesOnTheMainPage(){
+    let  htmlOflistQuizzes = document.querySelector('.all-quizzes__list-quizzes')
+    ALLQUIZZESLIST.forEach(quizz=>{
+        htmlOflistQuizzes.innerHTML += `
+        <figure id=${quizz.id} class="all-quizzes__list-quizzes__quizz" onclick="openThisQuizz(id)">
+            <img src="${quizz.image}">
+            <figcaption>${quizz.title}</figcaption>
+        </figure>
+        `        
+    })
+}
+
+function waitingForServerResponse(){
+    if(ALLQUIZZESLIST.length === 50){
+        stopWaiting()
+        insertQuizzesOnTheMainPage()
+    }
+}
+
+function saveQuizzesToAList(quizzes){
+    ALLQUIZZESLIST = quizzes.data
+}
+
+function showRequestErr(requestErr){
+    console.log(requestErr)
+}
+
+function serverRequest(){
+    let request = axios.get(SERVERLINK)
+    request.then(saveQuizzesToAList)
+    request.catch(showRequestErr)
+}
+
+function insertQuizzesOnThePage(){
+    serverRequest()
+    requestReceived = setInterval(waitingForServerResponse, 500)
+}
+//  --------------------------------------------------------
+
+// ============= main code =================== // 
+insertQuizzesOnThePage()
---
+++ js/playQuizzConfigs.js
@@ -0,0 +1,5 @@
+function openThisQuizz(idElement){
+    console.log(idElement)
+}
+
+console.log('oi')
