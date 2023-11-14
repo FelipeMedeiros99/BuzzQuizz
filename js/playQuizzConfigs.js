514b9f1108b638df4037be0c16a0ca0ad1200b3d
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2023-11-14 18:56:52 -0300

    elementos da tela do quizz adicionados via js. falta personalisar no css e adicionar interação com usuário

--- js/playQuizzConfigs.js
+++ js/playQuizzConfigs.js
@@ -1,9 +1,10 @@
 // vars ----------------------------------------------------
 let selectedQuizz = ''
-
+let activeQuizzClass = document.querySelector('.active-quizz')
+let answers = []
+let questions = []
 // opening the quizz ---------------------------------------
 function createTitleQuizz(){
-    let activeQuizzClass = document.querySelector('.active-quizz')
     activeQuizzClass.innerHTML = `
     <figure class="active-quizz__header">
         <img src="${selectedQuizz.image}">
@@ -12,12 +13,48 @@ function createTitleQuizz(){
     `
 }
 
-function hideMainPage(){
-    document.querySelector('.all-quizzes').classList.add('oculto')
+function insertingQuestionsInHTML(){ 
+    selectedQuizz.questions.forEach(question =>{
+        activeQuizzClass.innerHTML+=`
+        <div class="active-quizz__questions-box">
+            <h3 style="background-color: ${question.color}">${question.title}</h3>
+            <div class="active-quizz__answers-box"></div>
+        </div>  
+        `
+    })
+}
+
+function savingQuestionsAndAnswers(){
+    let questionsQuizz = selectedQuizz.questions 
+    questionsQuizz.forEach(question => {
+        answers.push(question.answers)
+    })
+}
+
+function insertingAnswersInHTML(){
+    let questionsBox = document.querySelectorAll('.active-quizz__answers-box')
+    for(let i=0; i< questionsBox.length; i++){
+        answers[i].forEach(answer=>{
+            questionsBox[i].innerHTML+= `
+            <figure class="active-quizz__questions-box__answers">
+                <img class="alternative" src="${answer.image}">
+                <figcaption>${answer.text}</figcaption>
+            </figure>
+            `
+        })
+    }
+}
+
+function createQuizzBoxQuestionsAndAnswer(){
+    savingQuestionsAndAnswers()
+    insertingQuestionsInHTML()
+    insertingAnswersInHTML()
+
 }
 
 function insertTheSelectedQuizzOnTheActiveQuizzPage(){
     createTitleQuizz()
+    createQuizzBoxQuestionsAndAnswer()
 }
 
 function saveTheSelectedQuizz(idElement){
@@ -30,7 +67,7 @@ function saveTheSelectedQuizz(idElement){
 }
 
 function openThisQuizz(idElement){
-    hideMainPage()
+    hideElement('.all-quizzes')
     saveTheSelectedQuizz(idElement)    
     insertTheSelectedQuizzOnTheActiveQuizzPage()
     console.log(selectedQuizz)
